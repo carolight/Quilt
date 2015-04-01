f7ac33804f3eed0f79b28c81f8ec29a05f3812fb
carolight <Caroline@Carolines-MacBook-Pro.local>
2015-04-01 13:48:07 +1000

    Initial Commit

---
+++ Quilt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quilt
+//
+//  Created by Caroline Begbie on 1/04/2015.
+//  Copyright (c) 2015 Caroline Begbie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
